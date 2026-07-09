0ebffe54650f9ba00bb003b9be70cf40d35d0524
cjt163 <charlietucker1919@gmail.com>
2026-07-09 14:06:21 -0400

    add matvec kernel for square matrix to vector multiplications. PMPP Ch3 exercises done. Now benchmark on 5070...

--- matmulv1_general.cu
+++ matmulv1_general.cu
@@ -1,5 +1,5 @@
 
-// N x M
+
 __global__ void matMulRowOut(float *A, float *B,float *C,int width) {
     int trow = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -7,9 +7,37 @@ __global__ void matMulRowOut(float *A, float *B,float *C,int width) {
         for (int i = 0; i < width; i++) {
             float outVal = 0.0;
             for (int j = 0; j < width; j++) {
-                outVal += A[i * width + j] * B[trow * width + j]; // sum(j) over A[i][j] * B[trow][j]
+                outVal += A[trow* width + j] * B[j * width + i]; // sum(j) over A[trow][j] * B[j][i]
             }
             C[trow * width + i] = outVal;
         }
     }
 }
+
+__global__ void matMulColOut(float *A, float *B,float *C,int width) {
+    int tcol = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (tcol < width) {
+        for (int i = 0; i < width; i++) {
+            float outVal = 0.0;
+            for (int j = 0; j < width; j++) {
+                outVal += A[i* width + j] * B[j * width + tcol]; // sum(j) over A[i][j] * B[j][tcol]
+            }
+            C[i * width + tcol] = outVal;
+        }
+    }
+}
+
+
+__global__ void matvec(float * A, float *x, float * out, int width) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (tid < width) {
+        float psum =0.0;
+        for (int j = 0; j < width; j++) {
+            psum += A[tid* width + j] * x[j]; // sum(j) over A[tid][j] * x[j]
+        }
+        out[tid] = psum;
+    }
+}
+
